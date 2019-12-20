1546ab7b2d2321f70a07624b50fd34613227aad1
Zachary <zachary.zboncak@gmail.com>
2019-12-19 22:42:54 -0600

    added filtering by query parameters

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ const movieList = require('./movieList.json')
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors())
-app.use(helmet())
+app.use(cors());
+app.use(helmet());
 
 function handleMovieRequest(req, res){
-    res.send(movieList)
+    const { genre, country, avg_vote } = req.query;
+
+    let currentMovies = movieList.slice(0);
+
+    if(genre) {
+        currentMovies = currentMovies.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+    }
+
+    if(country) {
+        currentMovies = currentMovies.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
+    }
+
+    if(avg_vote) {
+        currentMovies = currentMovies.filter(movie => movie.avg_vote >= avg_vote);
+    }
+
+    res.send(currentMovies)
 }
 
-app.get('/movie',handleMovieRequest)
+app.get('/movie', handleMovieRequest)
 
 
 module.exports = app;
